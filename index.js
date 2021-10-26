753b0ad2294270a51607d0d38db7f8bb22fa563f
yoyo930021 <yoyo930021@gmail.com>
2021-10-26 21:03:29 +0800

    fix: wrong regex

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const dynamicResourceUnplugin = createUnplugin((options, meta) => {
 
   const debug = options?.debug ?? false
   const fn = options?.fn ?? '__dynamicResource'
-  const regex = new RegExp(`${fn}\\s*\\(\\s*(\`([./\\w]|\$\{[\\w.()]+\})+\`|['"][./\\w+ \s'"]+['"]|['"][\\w/.]+['"]\\.concat\\([/\\w, '".()]+\\))\\s*,\\s*['"\`]__resource__['"\`]\\s*\\)`, 'gm')
+  const regex = new RegExp(`${fn}\\s*\\(\\s*(\`([./\\w]|\\$\{[\\w.()]+\})+\`|['"][./\\w+ \s'"]+['"]|['"][\\w/.]+['"]\\.concat\\([/\\w, '".()]+\\))\\s*,\\s*['"\`]__resource__['"\`]\\s*\\)`, 'gm')
   const replacer = (() => {
     if (meta.framework === 'webpack') return (stat) => `require(${stat})${options?.esModule ? '.default' : ''}`
     if (meta.framework === 'vite') return (stat) => `new URL(${stat}, import.meta${''}.url).href`
