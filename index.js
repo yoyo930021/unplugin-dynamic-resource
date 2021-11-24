3d4313614d59965c41977d948738f131f2cc642a
yoyo930021 <yoyo930021@gmail.com>
2021-11-24 12:04:56 +0800

    fix: corner case

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const dynamicResourceUnplugin = createUnplugin((options, meta) => {
   const regex = new RegExp(`${fn}\\s*\\(\\s*(.*)\\s*,\\s*['"\`]__resource__['"\`]\\s*\\)`, 'gm')
   const replacer = (() => {
     if (meta.framework === 'webpack') return (stat) => `require(${stat})${options?.esModule ? '.default' : ''}`
-    if (meta.framework === 'vite') return (stat) => `new URL(${stat}, import.meta${''}.url).href`
+    if (meta.framework === 'vite') return (stat) => `new URL(${stat}, import${''}.meta${''}.url).href`
   })()
 
   return {
